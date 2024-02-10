7d63278b8e89900f0f06a5f2b39c8ad14ade79d6
BrunaDeAraujo <baby.blue3940@gmail.com>
2024-02-10 17:33:32 -0300

    final

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 2;
-console.log(numeroSecreto)
- let escolhido = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(numeroSecreto);
+let escolhido;
+let tentativas = 1;
 
-if (numeroSecreto == escolhido) {
-    alert(`Parabéns! Você descobriu o número secreto: ${numeroSecreto}`);
-} else{
-    alert('Voce errou :(')
+while (escolhido != numeroSecreto) {
+    escolhido = parseInt(prompt('Escolha um número entre 1 e 100'));
+
+    if (numeroSecreto == escolhido) {
+        break;
+    } else {
+        if (numeroSecreto > escolhido) {
+            alert(`O número secreto é maior que ${escolhido}`);
+        } else {
+            alert(`O número secreto é menor que ${escolhido}`);
+        }
+        tentativas++;
+    }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
